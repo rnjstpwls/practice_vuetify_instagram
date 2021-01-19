70c9675b6f800652960c6a2ad5883b5e2667cfe9
권세진 <kwon_sejin@naver.com>
2021-01-19 21:12:59 +0900

    main page create

---
+++ src/components/main/Article.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>MyNameIsArticle</h1>
+    <UsernameBar />
+    <Photo />
+  </div>
+</template>
+
+<script>
+import Photo from './Photo.vue'
+import UsernameBar from "./UsernameBar.vue"
+
+export default {
+  name : 'Article',
+  components: {
+    UsernameBar,
+    Photo
+  }
+}
+    
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/main/Photo.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card
+    class="mx-3"
+    tile
+    max-height="200"
+  >
+    <v-carousel 
+      :show-arrows="false"
+      hide-delimiter-background
+      hide-delimiters
+    >
+      <v-carousel-item
+        v-for="(item, idx) in items"
+        :key="idx"
+        :src="item.src"
+        max-height="200"
+      ></v-carousel-item>
+    </v-carousel>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Photo',
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
